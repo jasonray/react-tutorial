553ea86a976c25041b36d0afdc8fa56467fe31e9
Jason Ray <jason.ray@ablevets.com>
2019-12-09 14:04:47 -0500

    pass props value to square

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -34,7 +34,7 @@ class Board extends React.Component {
           {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
+            {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
